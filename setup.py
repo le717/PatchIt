985d5d47577b834fb374df90665a56d25df7cc67
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-02 16:02:52 -0500

    Forgot to update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from cx_Freeze import setup, Executable
 
 setup(
-    name = "LEGO Racers Nyan Cat-Athon Mod",
-    version = "Unknown",
-    description = "Compressed with LEGO Racers Mod Installer by le717",
-	author = "JimbobJeffers and le717",
+    name = "PatchIt!",
+    version = "1.0 Beta 1",
+    description = "The simple and standard way to create and install LEGO Racers mods.",
+	author = "le717",
 	license = "GNU GPLv3",
-    executables = [Executable("LRModInstaller.py")]
+    executables = [Executable("PatchIt.py")]
 )
