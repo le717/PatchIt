01c4c164f8b816d454154b6db014b8d98ac9a953
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-02 16:05:19 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from cx_Freeze import setup, Executable
 setup(
     name = "PatchIt!",
     version = "1.0 Beta 2",
-    description = "Standard way to create and install LEGO Racers mods.",
+    description = "The simple and standard way to create and install LEGO Racers mods.",
 	author = "le717",
 	license = "GNU GPLv3",
     executables = [Executable("PatchIt.py")]
-)
