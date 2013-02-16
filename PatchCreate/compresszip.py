30d0953fae5ade08e1f4ba34e08f58a65ea8f102
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-15 20:42:27 -0500

    I thought I formatted it already...

--- PatchCreate/compresszip.py
+++ PatchCreate/compresszip.py
@@ -3,10 +3,8 @@ import os, shutil
 def compressfiles():
     '''Compress PatchIt! patch'''
     global zipfile, inputfiles
-
     print()
     inputfiles = input("Please enter the path to the files you wish to compress: \n\n> ")
-    
     zipfile = shutil.make_archive(inputfiles, format="zip", root_dir=inputfiles)
         # TODO: Move Patch File from app directory
     #newzipfile = os.replace(zipfile, "{0}{1}.zip".format(name, ver))
