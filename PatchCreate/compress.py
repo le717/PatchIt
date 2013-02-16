680a69439705f8f1d069a8a4965f4aa9112a3ecb
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-16 18:56:03 -0500

    Now to move the files out of the app directory, and tell
    the extractor to look in the same folder as the patch to find the ZIP

--- PatchCreate/compress.py
+++ PatchCreate/compress.py
@@ -2,7 +2,7 @@ import PatchIt
 import os, shutil
 
 def writepatch():
-    global createname
+    global createname, createver
     createname = input("Name: ")
     createver = input("Version: ")
     createauthor = input("Author: ")
@@ -25,18 +25,18 @@ def compressfiles():
     print()
     inputfiles = input("Please enter the path to the files you wish to compress: \n\n> ")
     zipfile = shutil.make_archive(inputfiles, format="zip", root_dir=inputfiles)
+    newzipfile = os.rename(zipfile, createname + createver + ".zip")
+    #shutil.move(newzipfile, inputfiles)
+    #shutil.move(createpatch, inputfiles)
         # TODO: Move Patch File from app directory
     #newzipfile = os.replace(zipfile, "{0}{1}.zip".format(name, ver))
 
     if os.system(inputfiles) == 1:
         print("{0} patch for {1} created and saved to {2}.zip".format(PatchIt.app, createname, inputfiles))
         PatchIt.main()
-        #return True
     elif os.system(inputfiles) == 0:
         print("Creation of {0} patch for {1} ended with an unknown error. Please try again.".format(PatchIt.app, createname))
         PatchIt.main()
-        #return False
     else:
         print("Creation of {0} patch for {1} failed!".format(PatchIt.app, createname))
         PatchIt.main()
-        #return "Fail"
