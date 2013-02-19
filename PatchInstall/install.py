5db1cb274a30bdc68d636b94c98a0652944812f0
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-18 20:15:09 -0500

    What is happening?! My test script extracts to the path
    defined in settings perfectly, but this won't, and it's the same code!
    AHHHHH!

--- PatchInstall/install.py
+++ PatchInstall/install.py
@@ -1,11 +1,9 @@
-import linecache, random, zipfile
-#import gametips
+import linecache, random, zipfile, os
+from .gametips import gametips
 import PatchIt
 def readpatch():
     '''Reads and Installs PatchIt! Patch'''
-    #global installpatch
     linecache.clearcache()
-    #global installpatch
     installpatch = input("\nPlease enter the path to a {0} patch:\n\n> ".format(PatchIt.app))
     if installpatch.lower() == "exit":
         print("\nCanceling installation...")
@@ -15,34 +13,34 @@ def readpatch():
         if confirmpatch != "// PatchIt! Patch format, created by le717 and rioforce.\n": # Validity check
             print(confirmpatch, installpatch + " is not a valid {0} patch.".format(PatchIt.app))
         else:
-            #global modinstallname
-            modinstallname = linecache.getline(installpatch, 3)
-            modinstallver = linecache.getline(installpatch, 4)
-            modinstallauthor = linecache.getline(installpatch, 5)
-            modinstalldesc = linecache.getline(installpatch, 7)
-            print("\n{0} {1} {2} {3}".format(modinstallname, modinstallver, modinstallauthor, modinstalldesc))
-            print("Do you wish to install {0}".format(modinstallname), end="")
+            installname = linecache.getline(installpatch, 3)
+            installver = linecache.getline(installpatch, 4)
+            installauthor = linecache.getline(installpatch, 5)
+            installdesc = linecache.getline(installpatch, 7)
+            print("\n{0} {1} {2} {3}".format(installname, installver, installauthor, installdesc))
+            print("Do you wish to install {0}".format(installname), end="")
             confirminstall = input("\n> ")
             if confirminstall.lower() != "y":
-                print("\nCanceling installation of {0} {1}".format(modinstallname, modinstallver))
+                print("\nCanceling installation of {0} {1}".format(installname, installver))
                 PatchIt.main()
             else:
-                #installfiles()
                 linecache.clearcache()
                 installpath = linecache.getline('../settings', 2)
-                installpath = installpath.rstrip()
+                installpath = installpath.rstrip("\n")
                 installzipfile = linecache.getline(installpatch, 9)
-                installzipfile = installzipfile.rstrip()
+                installzipfile = installzipfile.rstrip("\n")
+                ziplocation = installpatch.rstrip("{0}{1}{2}".format(installname, installver, ".PiP"))
                 #print('\n"' + random.choice(gametips.gametips) + '"\n')
-                zip = zipfile.ZipFile(installpatch + installzipfile)
-                zip.extractall(installpath)
-                zipfile.ZipFile.close(zip)
-                if os.system(installpath) == 0: # TODO: Disregard OS error and use only app error, thus bringing the proper exit codes.
-                    print("{0} sucessfully installed!".format(modinstallname))
-                    PatchIt.main()
-                elif os.system(installpath) == 1:
-                    print("An unknown error occured while installing {0}.".format(modinstallname))
-                    PatchIt.main()
-                else:
-                    print("Installation of {0} failed!".format(modinstallname))
-                    PatchIt.main()
+                zip_handler = zipfile.ZipFile(ziplocation + installzipfile, "r")
+                zip_handler.extractall(path=installpath)
+                #zipfile.ZipFile.close(zip)
+
+            if os.system(installpath) == 0: # TODO: Disregard OS error and use only app error, thus bringing the proper exit codes.
+                print("{0} sucessfully installed!".format(installname))
+                PatchIt.main()
+            elif os.system(installpath) == 1:
+                print("An unknown error occured while installing {0}.".format(installname))
+                PatchIt.main()
+            else:
+                print("Installation of {0} failed!".format(installname))
+                PatchIt.main()
