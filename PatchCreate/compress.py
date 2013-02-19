3fb2857048219ab52155110cf9f0e27a02dc280d
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-19 14:51:32 -0500

    Can't seem to kill Windows's "is not a known command, blah, blah" error
    The only thing that stops it is WindowsError, but I can not use it,
    because it continues the patch creation process.

--- PatchCreate/compress.py
+++ PatchCreate/compress.py
@@ -3,7 +3,9 @@ import os, time, shutil
 
 def writepatch():
     '''Writes and compresses PatchIt! Patch'''
-    createname = input("Name: ")
+    print("\nCreate a {0} Patch".format(PatchIt.app), end="\n")
+    print('Type "exit" in the "Name:" field to cancel.', end="\n")
+    createname = input("\nName: ")
     if createname.lower() == "exit":
         print("\nCanceling creation...")
         time.sleep(0.5)
@@ -27,17 +29,21 @@ def writepatch():
     inputfiles = input("\nPlease enter the path to the files you wish to compress: \n\n> ")
     zipfile = shutil.make_archive(inputfiles, format="zip", root_dir=inputfiles)
     newzipfile = os.rename(zipfile, createname + createver + ".zip")
+    #fileslocation = inputfiles.rstrip("{0}{1}{2}".format(installname, installver, ".PiP"))
     #shutil.move(newzipfile, inputfiles)
     #shutil.move(createpatch, inputfiles)
         # TODO: Move Patch File from app directory
     #newzipfile = os.replace(zipfile, "{0}{1}.zip".format(name, ver))
+    time.sleep(0.5)
 
-    if os.system(inputfiles) == 1:
-        print("{0} patch for {1} created and saved to {2}.zip".format(PatchIt.app, createname, inputfiles))
-        PatchIt.main()
-    elif os.system(inputfiles) == 0:
-        print("Creation of {0} patch for {1} ended with an unknown error. Please try again.".format(PatchIt.app, createname))
-        PatchIt.main()
-    else:
-        print("Creation of {0} patch for {1} failed!".format(PatchIt.app, createname))
-        PatchIt.main()
+    if IOError:
+        print("")
+        if os.system(inputfiles) == 0:
+            print("{0} patch for {1} created and saved to {2}".format(PatchIt.app, createname, inputfiles))
+            PatchIt.main()
+        elif os.system(inputfiles) == 1:
+            print("Creation of {0} patch for {1} ended with an unknown error. Please try again.".format(PatchIt.app, createname))
+            PatchIt.main()
+        else:
+            print("Creation of {0} patch for {1} failed!".format(PatchIt.app, createname))
+            PatchIt.main()
