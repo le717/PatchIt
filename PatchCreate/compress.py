3e90b2d3176a344bf696934f9ef63977cab2cba9
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-19 20:29:44 -0500

    Moved patch and ZIP from app path to inputfiles, selective import
    TODO: UPDATE HTML README!!!!

--- PatchCreate/compress.py
+++ PatchCreate/compress.py
@@ -1,7 +1,8 @@
 # Import only certain items instead of "the whole toolbox"
 from PatchIt import (app, main)
 from time import sleep
-import os, shutil
+from shutil import (make_archive, move)
+from os import (system, replace)
 # GUI! :D
 import tkinter
 from tkinter import filedialog
@@ -40,7 +41,6 @@ def writepatch():
         print("{0}{1}.zip".format(createname, createver), file=createpatch, end="")
 
     # The files to be compressed
-    #inputfiles = input("\nPlease enter the path to the files you wish to compress: \n\n> ")
     root = tkinter.Tk()
     root.withdraw()
     inputfiles = filedialog.askdirectory(title="Select the files you wish to compress:")
@@ -48,30 +48,35 @@ def writepatch():
         print("\nCannot find any files to compress!")
         sleep(1)
         main()
+
     else:
     # Compresses the files
-        zipfile = shutil.make_archive(inputfiles, format="zip", root_dir=inputfiles)
+        zipfile = make_archive(inputfiles, format="zip", root_dir=inputfiles)
         # Rename the ZIP archive to createname + creationver, as defined in Documentation/PiP Format.md
-        newzipfile = os.rename(zipfile, createname + createver + ".zip")
+        newzipfile = replace(zipfile, createname + createver + ".zip")
 
-    # TODO: Move PiP file and archive from app directory to inputfiles
-    #shutil.move(newzipfile, inputfiles)
-    #shutil.move(createpatch, inputfiles)
-    #newzipfile = os.replace(zipfile, "{0}{1}.zip".format(name, ver))
+        # Define the Patch and ZIP filenames
+        patchfile = "{0}{1}.PiP".format(createname, createver)
+        newzipfile = "{0}{1}.zip".format(createname, createver)
 
+        # Move the Patch and ZIP to the folder the compressed files came from
+        movepatch = move(patchfile, inputfiles)
+        movepatch = move(newzipfile, inputfiles)
         sleep(0.5)
+
         '''Windows continually throws up the '*inputfiles* is not recognized as an internal or external command,
          operable program or batch file.' error, and I am unable to neither silence it nor hide it with looping back over all the code
          or killing the exit codes. So I had to redefine what is a clean exit and what isn't. Thus,
          1 == clean exit, 0, == exit with some error, and anything else is pure fail.
          Hopefully, I can fix this in Beta 4.'''
-        if os.system(inputfiles) == 1:
+
+        if system(inputfiles) == 1:
             print("\n{0} patch for {1} Version {2} created and saved to {3}!".format(app, createname, createver, inputfiles))
             # Always sleep for 1 second before kicking back to the menu.
             sleep(1)
             main()
 
-        elif os.system(inputfiles) == 0:
+        elif system(inputfiles) == 0:
             print("\nCreation of {0} patch for {1} Version {2} completed with an unknown error.".format(app, createname, createver))
             sleep(1)
             main()
