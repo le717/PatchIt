798e2b52d5954a9cb7d29302b7c67ca56669b60c
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-19 19:45:37 -0500

    Added Folder Select GUI into Patch Compression

--- PatchCreate/compress.py
+++ PatchCreate/compress.py
@@ -2,6 +2,9 @@
 from PatchIt import (app, main)
 from time import sleep
 import os, shutil
+# GUI! :D
+import tkinter
+from tkinter import filedialog
 
 # ------------ Begin PatchIt! Patch Creation ------------ #
 
@@ -37,37 +40,45 @@ def writepatch():
         print("{0}{1}.zip".format(createname, createver), file=createpatch, end="")
 
     # The files to be compressed
-    inputfiles = input("\nPlease enter the path to the files you wish to compress: \n\n> ")
+    #inputfiles = input("\nPlease enter the path to the files you wish to compress: \n\n> ")
+    root = tkinter.Tk()
+    root.withdraw()
+    inputfiles = filedialog.askdirectory(title="Select the files you wish to compress:")
+    if len(inputfiles) == 0:
+        print("\nCannot find any files to compress!")
+        sleep(1)
+        main()
+    else:
     # Compresses the files
-    zipfile = shutil.make_archive(inputfiles, format="zip", root_dir=inputfiles)
-    # Rename the ZIP archive to createname + creationver, as defined in Documentation/PiP Format.md
-    newzipfile = os.rename(zipfile, createname + createver + ".zip")
+        zipfile = shutil.make_archive(inputfiles, format="zip", root_dir=inputfiles)
+        # Rename the ZIP archive to createname + creationver, as defined in Documentation/PiP Format.md
+        newzipfile = os.rename(zipfile, createname + createver + ".zip")
 
     # TODO: Move PiP file and archive from app directory to inputfiles
     #shutil.move(newzipfile, inputfiles)
     #shutil.move(createpatch, inputfiles)
     #newzipfile = os.replace(zipfile, "{0}{1}.zip".format(name, ver))
 
-    sleep(0.5)
-    '''Windows continually throws up the '*inputfiles* is not recognized as an internal or external command,
-    operable program or batch file.' error, and I am unable to neither silence it nor hide it with looping back over all the code
-    or killing the exit codes. So I had to redefine what is a clean exit and what isn't. Thus,
-    1 == clean exit, 0, == exit with some error, and anything else is pure fail.
-   Hopefully, I can fix this in Beta 4.'''
-    if os.system(inputfiles) == 1:
-        print("\n{0} patch for {1} Version {2} created and saved to {3}!".format(app, createname, createver, inputfiles))
-        # Always sleep for 1 second before kicking back to the menu.
-        sleep(1)
-        main()
+        sleep(0.5)
+        '''Windows continually throws up the '*inputfiles* is not recognized as an internal or external command,
+         operable program or batch file.' error, and I am unable to neither silence it nor hide it with looping back over all the code
+         or killing the exit codes. So I had to redefine what is a clean exit and what isn't. Thus,
+         1 == clean exit, 0, == exit with some error, and anything else is pure fail.
+         Hopefully, I can fix this in Beta 4.'''
+        if os.system(inputfiles) == 1:
+            print("\n{0} patch for {1} Version {2} created and saved to {3}!".format(app, createname, createver, inputfiles))
+            # Always sleep for 1 second before kicking back to the menu.
+            sleep(1)
+            main()
 
-    elif os.system(inputfiles) == 0:
-        print("\nCreation of {0} patch for {1} Version {2} completed with an unknown error.".format(app, createname, createver))
-        sleep(1)
-        main()
+        elif os.system(inputfiles) == 0:
+            print("\nCreation of {0} patch for {1} Version {2} completed with an unknown error.".format(app, createname, createver))
+            sleep(1)
+            main()
 
-    else:
-        print("\nCreation of {0} patch for {1} Version {2} failed!".format(app, createname, createver))
-        sleep(1)
-        main()
+        else:
+            print("\nCreation of {0} patch for {1} Version {2} failed!".format(app, createname, createver))
+            sleep(1)
+            main()
 
 # ------------ End PatchIt! Patch Creation ------------ #
