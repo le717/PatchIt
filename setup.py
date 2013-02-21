89c14c294e02e36ea871d9b3def736b73cb1c3c0
le717 <mustIgiveoutmyemail@Iwillnotgiveitout.ok>
2013-02-21 14:02:46 -0500

    Tagging PatchIt! 1.0 Stable

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "PatchIt!",
-    version = "1.0 Beta 2",
-    description = "The simple and standard way to create and install LEGO Racers mods.",
-	author = "le717",
-	license = "GNU GPLv3",
-    executables = [Executable("PatchIt.py")]
