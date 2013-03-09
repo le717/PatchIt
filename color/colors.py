9a5854b1fc4623df273c0d04a2501cdfd84d2d8a
le717 <le717@e.mail>
2013-03-09 17:04:01 -0500

    Fixed script to allow compile via setup.py
    Well, almost. Now I get an error from somewhere else! D:

--- color/colors.py
+++ color/colors.py
@@ -11,7 +11,7 @@ def get_csbi_attributes(handle):
     import struct
     csbi = ctypes.create_string_buffer(22)
     res = ctypes.windll.kernel32.GetConsoleScreenBufferInfo(handle, csbi)
-    assert res
+##    assert res
 
     (bufx, bufy, curx, cury, wattr,
     left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
@@ -19,10 +19,10 @@ def get_csbi_attributes(handle):
 
 def color(text, color, nl = True):
     ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
-    sys.stdout.write(text)
+    print(text)
     ctypes.windll.kernel32.SetConsoleTextAttribute(handle, reset)
     if nl: print("")
-    sys.stdout.flush()
+##    sys.stdout.flush()
 
 handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
 reset = get_csbi_attributes(handle)
