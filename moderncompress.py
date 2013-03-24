6fc218ddbd5d33560a053738e20be011f3fac94b
le717 <le717@e.mail>
2013-03-23 21:09:20 -0400

    Updated GPL block

--- moderncompress.py
+++ moderncompress.py
@@ -1,4 +1,6 @@
 """
+    This file is part of PatchIt!
+
     PatchIt! -  the standard yet simple way to packaging and install mods for LEGO Racers
     Copyright 2013 Triangle717 <http://triangle717.wordpress.com>
 
@@ -7,14 +9,13 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    PatchIt! is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+    along with PatchIt!  If not, see <http://www.gnu.org/licenses/>.
 """
 # PatchIt! V1.1.0 Unstable Modern Patch Creation code
 
