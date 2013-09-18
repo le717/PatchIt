42cf8b11fd58b03c69f01afb29d2917a5003786b
Triangle717 <le717@e.mail>
2013-09-17 21:01:26 -0400

    Created script to download the newest copy of the readme from the gh-pages branch.

---
+++ Tools/bin/readme.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+import distutils.dir_util
+import zipfile
+
+# Import wget
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parentdir)
+import wget
+
+curDir = os.getcwd()
+link = "https://github.com/le717/PatchIt/archive/gh-pages.zip"
+name = "PatchIt-gh-pages.zip"
+#FIXME: os.path.dirname(os.path.dirname())? Hacky.
+path = os.path.join("{0}".format(os.path.dirname(os.path.dirname(curDir))),
+                    "Documentation", "Readme")
+
+# Download the readme from the gh-pages branch
+wget.download(link)
+
+# Remove the directory for a clean slate
+if os.path.exists(path):
+    distutils.dir_util.remove_tree(path)
+
+# Extract the zip to the proper location
+with zipfile.ZipFile(name, "r") as extract:
+    extract.extractall(path=path)
+
+# Delete the zip archive
+os.unlink(name)
+
+# Change the cwd to where we extracted the files to
+os.chdir(path)
+
+# Copy the files out of the subfolder
+distutils.dir_util.copy_tree(os.path.join(path, "PatchIt-gh-pages"), path)
+
+# Remove all the unneeded files/folders
+distutils.dir_util.remove_tree(os.path.join(path, "PatchIt-gh-pages"))
+distutils.dir_util.remove_tree(os.path.join(path, "Documentation"))
+distutils.dir_util.remove_tree(os.path.join(path, "Site-Project"))
+os.unlink(".gitignore")
+os.unlink(".gitattributes")
+
+# And we're done!
+print("\n\nDone. :)")
+raise SystemExit(0)
