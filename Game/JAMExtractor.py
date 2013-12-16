5869a6f7f968603ff291c095e8961c7a3c4c9567
Triangle717 <le717@hotmail.com>
2013-12-16 12:52:01 -0500

    Lots of PEP 8 to build-in JAM Extractor

--- Game/JAMExtractor.py
+++ Game/JAMExtractor.py
@@ -25,21 +25,22 @@
 import os
 import sys
 
+
 def extract(path, verbose):
     def uint32(offset):
         if bytesAre == "str":
-            return ord(fileData[offset]) + (ord(fileData[offset+1]) * 256) + (ord(fileData[offset+2]) * 65536) + (ord(fileData[offset+3]) * 16777216)
+            return ord(fileData[offset]) + (ord(fileData[offset + 1]) * 256) + (ord(fileData[offset + 2]) * 65536) + (ord(fileData[offset + 3]) * 16777216)
         else:
-            return fileData[offset] + (fileData[offset+1] * 256) + (fileData[offset+2] * 65536) + (fileData[offset+3] * 16777216)
+            return fileData[offset] + (fileData[offset + 1] * 256) + (fileData[offset + 2] * 65536) + (fileData[offset + 3] * 16777216)
 
     def listFolders(offset, number, folderPath):
-        #List of folder to build.
+        # List of folder to build.
         folderList = []
-        #Read in data for the total number of folder.
+        # Read in data for the total number of folder.
         for _ in range(number):
-            #Add ascii bytes to the end of the path until we hit a null byte.
+            # Add ascii bytes to the end of the path until we hit a null byte.
             name = folderPath
-            for b in fileData[offset:offset+12]:
+            for b in fileData[offset:offset + 12]:
                 if b == 0 or b == b"\x00":
                     break
                 if bytesAre == "str":
@@ -48,58 +49,58 @@ def extract(path, verbose):
                     name += chr(b)
             position = uint32(offset + 12)
             if verbose:
-                print("READING: " + name + "  OFFSET: " + str(position))
-            #Add the folder data to the array to return.
+                print("READING: {0}  OFFSET {1}".format(name, position))
+            # Add the folder data to the array to return.
             folderList.append([name, position])
-            #Skip the offset forward for the next folder.
+            # Skip the offset forward for the next folder.
             offset += 16
         return folderList
 
     def listFiles(offset, number, folderPath):
-        #List of files to build.
+        # List of files to build.
         fileList = []
-        #Read in data for the total number of files.
+        # Read in data for the total number of files.
         for _ in range(number):
-            #Add ascii bytes to the end of the path until we hit a null byte.
+            # Add ascii bytes to the end of the path until we hit a null byte.
             name = folderPath + os.sep
-            for b in fileData[offset:offset+12]:
+            for b in fileData[offset:offset + 12]:
                 if b == 0 or b == b"\x00":
                     break
                 if bytesAre == "str":
                     name += b
                 else:
                     name += chr(b)
-            #Add the file data to the array to return.
+            # Add the file data to the array to return.
             position = uint32(offset + 12)
             size = uint32(offset + 16)
             if verbose:
                 print("READING: " + name + "  OFFSET: " + str(position) + "  SIZE: " + str(size))
             fileList.append([name, position, size])
-            #Skip the offset forward for the next file.
+            # Skip the offset forward for the next file.
             offset += 20
         return fileList
 
     def recurse(foldersList):
         folderList.extend(foldersList)
         for f in foldersList:
-            #How many files are there?
+            # How many files are there?
             totalFiles = uint32(f[1])
             if totalFiles == 0:
-                #That place has only folders, run this function again for the folders in that folder.
-                recurse(listFolders(f[1]+8, uint32(f[1]+4), f[0] + os.sep))
+                # That place has only folders, run this function again for the folders in that folder.
+                recurse(listFolders(f[1] + 8, uint32(f[1] + 4), f[0] + os.sep))
             else:
-                #That place has files, add them to the list to extract.
-                fileList.extend(listFiles(f[1]+4, uint32(f[1]), f[0]))
-                #This place may also have folders. Find place where folder count exists after file list ends.
+                # That place has files, add them to the list to extract.
+                fileList.extend(listFiles(f[1] + 4, uint32(f[1]), f[0]))
+                # This place may also have folders. Find place where folder count exists after file list ends.
                 folderCountPos = totalFiles * 20 + f[1] + 4
                 folderCount = uint32(folderCountPos)
                 if folderCount > 0:
                     recurse(listFolders(folderCountPos + 4, folderCount, f[0] + os.sep))
 
-    #Check if this version of Python treats bytes as int or str
+    # Check if this version of Python treats bytes as int or str
     bytesAre = type(b'a'[0]).__name__
 
-    #Open the file and read it in.
+    # Open the file and read it in.
     with open(path, "rb") as f:
         fileData = f.read()
 
@@ -109,61 +110,63 @@ def extract(path, verbose):
 
     print("Extracting, please wait.")
 
-    #Recurse through, creating a list of all the files.
+    # Recurse through, creating a list of all the files.
     folderList = []
     fileList = []
     recurse([["", 4]])
 
-    #Create output path from input path.
+    # Create output path from input path.
     if path[-4:].lower() == ".jam":
         outFolder = path[:-4]
     else:
         outFolder = path
 
-    #Move the old folder out of the way if it exists.
+    # Move the old folder out of the way if it exists.
     if os.path.exists(outFolder):
-        #Move to the first name not taken.
+        # Move to the first name not taken.
         i = 1
-        while os.path.exists(outFolder + "_" + str(i) + "_bak"):
+        while os.path.exists("{0}_{1}_bak".format(outFolder, i)):
             i += 1
-        renameFolder = outFolder + "_" + str(i) + "_bak"
-        print("NOTE: Existing extraction found.\nMOVING: " + outFolder + " -> " + renameFolder)
+        renameFolder = "{0}_{1}_bak".format(outFolder, i)
+        print("NOTE: Existing extraction found.\nMOVING: {0} -> {1}".format(
+              outFolder, renameFolder))
         os.rename(outFolder, renameFolder)
 
-        #Check that the file was moved out of the way.
+        # Check that the file was moved out of the way.
         if os.path.exists(outFolder):
-            #File still exists, something went wrong.
+            # File still exists, something went wrong.
             print("ERROR: Failed to move old extraction folder out of the way, files not extracted.")
             return False
 
     os.makedirs(outFolder)
 
-    #Create all the folders we need to extract to.
+    # Create all the folders we need to extract to.
     for a in folderList:
         if a[0] == "":
             continue
         if not os.path.exists(outFolder + a[0]):
             if verbose:
-                print("WRITING: " + a[0])
+                print("WRITING: {0}".format(a[0]))
             os.makedirs(outFolder + a[0])
 
-    #Loop through the list of files, saving them.
+    # Loop through the list of files, saving them.
     for a in fileList:
         if verbose:
             print("WRITING: " + a[0] + "  SIZE: " + str(a[2]))
         with open(outFolder + a[0], "wb") as f:
-            f.write(fileData[a[1]:a[1]+a[2]])
+            f.write(fileData[a[1]:a[1] + a[2]])
 
     print("COMPLETE: " + str(len(fileList)) + " files extracted.\nOUTPUT: " + outFolder)
     return True
 
+
 def build(path, verbose):
-    def writeUint32(i = 0, position = -1):
-        #Keep it possible.
+    def writeUint32(i=0, position = -1):
+        # Keep it possible.
         if i > 4294967295:
             i = 4294967295
         a = [0, 0, 0, 0]
-        #Roll it over to the next place until the end giving what's left to the first number.
+        # Roll it over to the next place until the end giving what's left to the first number.
         while i >= 16777216:
             a[3] += 1
             i -= 16777216
@@ -174,14 +177,14 @@ def build(path, verbose):
             a[1] += 1
             i -= 256
         a[0] = i
-        #If position not set, add it to the end.
+        # If position not set, add it to the end.
         if position < 0:
             fileData.extend(a)
         else:
-            fileData[position:position+4] = a
+            fileData[position:position + 4] = a
 
     def writeName(s):
-        #Write 12 bytes for the name.
+        # Write 12 bytes for the name.
         for i in range(12):
             if i < len(s):
                 fileData.append(ord(s[i]))
@@ -189,7 +192,7 @@ def build(path, verbose):
                 fileData.append(0)
 
     def updateFolder(s):
-        #Update the pointer to the folder.
+        # Update the pointer to the folder.
         for a in folderList:
             if a[0] == s:
                 writeUint32(len(fileData), a[1])
@@ -197,14 +200,14 @@ def build(path, verbose):
 
     print("Building, please wait.")
 
-    #Start the byte array.
+    # Start the byte array.
     fileData = bytearray(b"LJAM")
-    #Create lists to store file and folder pointers to update later.
+    # Create lists to store file and folder pointers to update later.
     folderList = []
     fileList = []
 
     for currentdir, dirlist, filelist in os.walk(path):
-        #Filter out files and folder with names that are too long for the format.
+        # Filter out files and folder with names that are too long for the format.
         for i in reversed(range(len(filelist))):
             if len(filelist[i]) > 12:
                 if verbose:
@@ -216,14 +219,14 @@ def build(path, verbose):
                     print("SKIPPING: " + dirlist[i] + "  (Name too long.)")
                 del dirlist[i]
 
-        #Update pointer to this folder if listed earlier.
+        # Update pointer to this folder if listed earlier.
         updateFolder(currentdir)
-        #Write the number of files.
+        # Write the number of files.
         writeUint32(len(filelist))
 
-        #Lists files.
+        # Lists files.
         for filename in filelist:
-            #Write to index, remembering where to update pointers later.
+            # Write to index, remembering where to update pointers later.
             a = [currentdir + os.sep + filename, 0, 0]
             writeName(filename)
             a[1] = len(fileData)
@@ -232,23 +235,23 @@ def build(path, verbose):
             writeUint32()
             fileList.append(a)
             if verbose:
-                print("READING: " + a[0][len(path):])
+                print("READING: {0}".format(a[0][len(path):]))
 
-        #Write the number of folders.
+        # Write the number of folders.
         writeUint32(len(dirlist))
 
-        #Lists directories.
+        # Lists directories.
         for subdirname in dirlist:
-            #Write to index, remembering where to update pointers later.
+            # Write to index, remembering where to update pointers later.
             a = [currentdir + os.sep + subdirname, 0]
             writeName(subdirname)
             a[1] = len(fileData)
             writeUint32()
             folderList.append(a)
             if verbose:
-                print("READING: " + a[0][len(path):])
+                print("READING: {0}".format(a[0][len(path):]))
 
-    #Append the files and update pointers.
+    # Append the files and update pointers.
     for a in fileList:
         start = len(fileData)
         writeUint32(start, a[1])
@@ -260,41 +263,42 @@ def build(path, verbose):
             writeUint32(size, a[2])
             fileData.extend(fBytes)
 
-    #Create the output file.
-    outFile = path + ".JAM"
+    # Create the output file.
+    outFile = "{0}.JAM".format(path)
 
-    #Move the old file out of the way if it exists.
+    # Move the old file out of the way if it exists.
     if os.path.exists(outFile):
-        #Move to the first name not taken.
+        # Move to the first name not taken.
         i = 1
-        while os.path.exists(outFile + "." + str(i) + ".bak"):
+        while os.path.exists("{0}.{1}.bak".format(outFile, i)):
             i += 1
-        renameFile = outFile + "." + str(i) + ".bak"
-        print("NOTE: Existing archive found.\nMOVING " + outFile + " -> " + renameFile)
+        renameFile = "{0}.{1}.bak".format(outFile, i)
+        print("NOTE: Existing archive found.\nMOVING {0} ->  {1}".format(
+              outFile, renameFile))
         os.rename(outFile, renameFile)
 
-        #Check that the file was moved out of the way.
+        # Check that the file was moved out of the way.
         if os.path.exists(outFile):
-            #File still exists, something went wrong.
+            # File still exists, something went wrong.
             print("ERROR: Failed to move old archive out of the way, archive not written.")
             return False
 
-    #Write the archive.
+    # Write the archive.
     with open(outFile, "wb") as f:
         f.write(fileData)
 
-    print("COMPLETE: Achive built.\nOUTPUT: " + outFile)
+    print("COMPLETE: Achive built.\nOUTPUT: {0}".format(outFile))
 
     return True
 
-#Detect if executable or not.
+# Detect if executable or not.
 fileName = sys.argv[0].split(os.sep).pop()
 if fileName[-3:] == ".py" or fileName[-4:] == ".pyw":
     runCommand = "python " + fileName
 else:
     runCommand = fileName
 
-#Preprocess arguments.
+# Preprocess arguments.
 fileList = []
 verbose = False
 for i in range(1, len(sys.argv)):
@@ -304,7 +308,7 @@ for i in range(1, len(sys.argv)):
     else:
         fileList.append(arg)
 
-#Process files/folders arrordingly or display message.
+# Process files/folders arrordingly or display message.
 if len(fileList) > 0:
     for i in fileList:
         if os.path.isfile(i):
